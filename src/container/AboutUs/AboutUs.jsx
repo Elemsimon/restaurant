c3f3dfcf0136555f47b7d4d30ff32efb6a2fe95c
Elemsimon <elemifesinachisimon@gmail.com>
2024-12-02 18:35:08 +0100

    update

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,34 +1,76 @@
-import React from 'react';
-
+import { useEffect, useState } from 'react';
+import { urlFor, client } from '../../client';
 import { images } from '../../constants';
 import './AboutUs.css';
 
-const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding" id="about">
-    <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="G_overlay" />
+const AboutUs = () => {
+  const [about, setAbout] = useState(null); 
+  const [error, setError] = useState(null);
+  const [history, setHistory] = useState(null); 
+
+  useEffect(() => {
+    const queryAbout = `*[_type == "about"][0]{title, body, imgUrl}`; // Query for data
+    const queryHistory = `*[_type == "history"][0]{title, body}`; // Query for data
+
+    // Fetch data
+    client
+      .fetch(queryAbout)
+      .then((data) => {
+        setAbout(data); // Set data
+      })
+      .catch((err) => {
+        console.error('Error fetching data:', err);
+        setError(err);
+      });
+
+      client
+      .fetch(queryHistory)
+      .then((data) => {
+        setHistory(data); // Set history data
+      })
+      .catch((err) => {
+        console.error('Error fetching data:', err);
+        setError(err);
+      });
+  }, []);
+    
+
+  if (error) {
+    return <div>Error loading content: {error.message}</div>;
+  }
+
+  // Display loading until data are fetched
+  if (!about) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="app__aboutus app__bg flex__center section__padding" id="about">
+      <div className="app__aboutus-overlay flex__center">
+        <img src={images.G} alt="G_overlay" />
+      </div>
+  
+      <div className="app__aboutus-content flex__center">
+        <div className="app__aboutus-content_about">
+          <h1 className="headtext__cormorant">{about.title}</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">{about.body}</p>
+          <button type="button" className="custom__button">Know More</button>
+        </div>
+  
+        <div className="app__aboutus-content_knife flex__center">
+          <img src={urlFor(about.imgUrl)} alt="about_knife" />
+        </div>
+  
+        <div className="app__aboutus-content_history">
+          <h1 className="headtext__cormorant">{history.title}</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">{history.body}</p>
+          <button type="button" className="custom__button">Know More</button>
+        </div>
+      </div>
     </div>
-
-    <div className="app__aboutus-content flex__center">
-      <div className="app__aboutus-content_about">
-        <h1 className="headtext__cormorant">About Us</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
-        <button type="button" className="custom__button">Know More</button>
-      </div>
-
-      <div className="app__aboutus-content_knife flex__center">
-        <img src={images.knife} alt="about_knife" />
-      </div>
-
-      <div className="app__aboutus-content_history">
-        <h1 className="headtext__cormorant">Our History</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
-        <button type="button" className="custom__button">Know More</button>
-      </div>
-    </div>
-  </div>
-);
+  );
+}
 
 export default AboutUs;
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,33 +1,64 @@
-import React from 'react';
-
+import { useEffect, useState } from 'react';
+import { urlFor, client } from '../../client';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Chef.css';
 
-const Chef = () => (
-  <div className="app__bg app__wrapper section__padding">
-    <div className="app__wrapper_img app__wrapper_img-reverse">
-      <img src={images.chef} alt="chef_image" />
-    </div>
-    <div className="app__wrapper_info">
-      <SubHeading title="Chef's word" />
-      <h1 className="headtext__cormorant">What we believe in</h1>
+const Chef = () => {
+  const [chef, setChef] = useState(null);
+  const [error, setError] = useState(null);
 
-      <div className="app__chef-content">
-        <div className="app__chef-content_quote">
-          <img src={images.quote} alt="quote_image" />
-          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+  useEffect(() => {
+    const queryChef = `*[_type == "chef"][0]{title, body, body2, name, position, sign, imgUrl}`; // Query for data
+
+    // Fetch data
+    client
+      .fetch(queryChef)
+      .then((data) => {
+        setChef(data); // Set data
+        console.log("Fetched data:", data);
+      })
+      .catch((err) => {
+        console.error('Error fetching data:', err);
+        setError(err);
+      });
+  }, []);
+
+  // Display error if it occurs
+  if (error) {
+    return <div>Error loading content: {error.message}</div>;
+  }
+
+  // Display loading until data are fetched
+  if (!chef) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="app__bg app__wrapper section__padding">
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+        <img src={urlFor(chef.imgUrl)} alt="chef_image" />
+      </div>
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef's word" />
+        <h1 className="headtext__cormorant">{chef.title}</h1>
+  
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <img src={images.quote} alt="quote_image" />
+            <p className="p__opensans">{chef.body}</p>
+          </div>
+          <p className="p__opensans"> {chef.body2} </p>
         </div>
-        <p className="p__opensans"> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
-      </div>
-
-      <div className="app__chef-sign">
-        <p>Kevin Luo</p>
-        <p className="p__opensans">Chef & Founder</p>
-        <img src={images.sign} alt="sign_image" />
+  
+        <div className="app__chef-sign">
+          <p>{chef.name}</p>
+          <p className="p__opensans">{chef.position}</p>
+          <img src={urlFor(chef.sign)} alt="sign_image" />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Chef;
